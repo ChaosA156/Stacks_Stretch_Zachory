696c465d09817d76abbc891b2e41bb98a2085e1f
ChaosA156 <ChaosA156@gmail.com>
2023-09-22 14:29:17 -0500

    Initial commit

---
+++ Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks_Stretch_Zachory
+{
+    //Some code with parameters for the cards and suit
+    class Card
+    {
+
+        public string Number { get; }
+
+        public string Suit { get; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+    }
+}
